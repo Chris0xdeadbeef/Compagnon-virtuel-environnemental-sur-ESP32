8eeca2782ec69f850db7272ebdf5771fbc03963c
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2026-05-13 10:15:11 +0200

    chore(main): test display the environmental data

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,36 @@
 #include <Arduino.h>
-void setup() {
+#include "Config.h"
+#include "Sensors.h"
+#include "Types.h"
+
+EnvironmentalData environmentalData;
+
+void setup()
+{
+    Serial.begin(SERIAL_BAUD_RATE);
+
+    if (!initSensors())
+    {
+        Serial.println("Erreur initialisation capteurs");
+
+        while (true) {}
+    }
 }
 
-void loop() {
+void loop()
+{
+    updateSensors(environmentalData);
+
+    Serial.print("Temperature: ");
+    Serial.print(environmentalData.temperature);
+    Serial.println(" C");
+
+    Serial.print("Humidite: ");
+    Serial.print(environmentalData.humidity);
+    Serial.println(" %");
+
+    Serial.print("Nuit: ");
+    Serial.println(environmentalData.isNight);
+
+    delay(300);
 }
